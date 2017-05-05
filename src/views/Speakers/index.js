54d9000b426ce5b17b86a60aad269c4622e75ffb
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-05-05 18:51:59 -0300

    Add animation for speakers

--- src/views/Speakers/index.js
+++ src/views/Speakers/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react'
+import React, {Component} from 'react'
 
 import './Speakers.sass'
 
@@ -11,18 +11,49 @@ import Speaker from './Speaker'
 // DATA ________________________________________________________________________
 import speakers from './data'
 
-function Speakers() {
-  return (
-    <Section id="speakers" name="Disertantes" className="background_orange speakers">
-      <div className="speakers__grid">
-        {
-          speakers.map((speaker, i) => (
-            <Speaker key={i} {...speaker}/>
-          ))
-        }
-      </div>
-    </Section>
-  )
+class Speakers extends Component {
+  state: {
+    shouldAppear: boolean
+  }
+
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      shouldAppear: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', (e) => {
+      const should = this.refs._grid.getBoundingClientRect().top < 250
+
+      if(should && !this.state.shouldAppear) {
+        console.log('triggered')
+        this.setState({
+          shouldAppear: true
+        })
+      }
+    })
+  }
+
+  render() {
+    const {
+      shouldAppear
+    } = this.state
+    return (
+      <Section id="speakers" name="Disertantes" className="background_orange speakers">
+        <div ref="_grid"
+          className={`speakers__grid ${shouldAppear ? 'speakers__grid--mount' : ''}`}>
+          {
+            speakers.map((speaker, i) => (
+              <Speaker key={i} {...speaker}/>
+            ))
+          }
+        </div>
+      </Section>
+    )
+  }
 }
 
 export default Speakers
