f44bafacf9047925076c104b2dc62fb8dc3b872a
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-05-05 12:36:55 -0300

    Add introduction text

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -2,13 +2,37 @@
 
 import React from 'react'
 
+import './About.sass'
+
 // COMPONENTS _________________________________________________________________
 import Section from '../../components/Section'
 
 function About() {
   return (
     <Section name="¿Qué es el Simposio?" className="background_white about">
-      some text here
+      <div className="about__portrait">
+        <div className="about__pic-container">
+          <div className="about__pic">
+          </div>
+        </div>
+      </div>
+      <div className="about__content">
+        <div className="introduction">
+          <h2>Introducción</h2>
+          <p>
+            Lorem ipsum dolor sit amet, no usu consul malorum periculis. Ea eos case apeirian, qui ut commodo alienum salutandi. Aeterno ponderum at vix. Sanctus accumsan similique ne has, in ignota numquam eam. Unum disputando cu has. Verear timeam et pri. Nec omittam consequuntur te.
+          </p>
+          <p>
+            Ei est causae aeterno gloriatur, cu delenit laoreet delectus per. Congue impedit ea nam. Ad graeci diceret albucius sed. Ius ei commune singulis, id graece platonem reprehendunt duo. No usu viris complectitur, cetero nonumes mel ut, tollit suscipiantur per no.
+          </p>
+          <p>
+            Tation lobortis facilisis est ex. Eius oratio at quo. Ea consul argumentum vim. In nam accumsan appareat. Pro exerci sensibus inimicus ne.
+          </p>
+          <p>
+            Quo no ubique alterum democritum, erant tritani ad eum, eius cotidieque eos in. Te mea rebum inermis, volumus definitionem ne mei. Option conclusionemque ei ius, graeci diceret reprimique ut nam, vim no ferri sapientem. Mutat accusam mel id, nostrud eleifend elaboraret per in, ne ius alterum probatus deterruisset.
+          </p>
+        </div>
+      </div>
     </Section>
   )
 }
