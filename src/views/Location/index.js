7c45fca29b9b665c4942bdcdaea3a462430647f3
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-05-09 12:51:45 -0300

    Add direction tile

--- src/views/Location/index.js
+++ src/views/Location/index.js
@@ -1,21 +1,29 @@
 import React from 'react'
 
+import './Location.sass'
+
+import map from './maps-2.jpg'
+
 // COMPONENTS __________________________________________________________________
 import Section from '../../components/Section'
+import SocialLinks from '../../components/SocialLinks'
 
 function Location() {
   return (
     <Section id="location" name="Ubicación" className="background_orange location">
       <article className="tile">
-        <h2>DIRECCIÓN</h2>
-        <p>Edificio tal de la Universidad de cual Calle tanto 12345, Ushuaia, Tierra del Fuego, Argentina.
-          <br/>
-          <a href="maps.google.com">Ver en Google Maps</a>
-        </p>
-        <div className="tile__social">
-          some links
+        <div className="tile__left">
+          <h2 className="tile__title">DIRECCIÓN</h2>
+          <p className="text-container tile__text">Edificio tal de la Universidad de cual<br/>Calle tanto 12345, Ushuaia, Tierra del Fuego, Argentina.
+          </p>
+          <p className="tile__text"><a href="maps.google.com">Ver en Google Maps</a></p>
+          <div className="tile__social">
+            <SocialLinks/>
+          </div>
         </div>
-        <img src="" alt=""/>
+        <div className="tile__right">
+          <img className="tile__map" src={map} alt="Mapa de la ubicacion"/>
+        </div>
       </article>
     </Section>
   )
