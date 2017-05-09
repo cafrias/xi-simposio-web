4f25ad50df5135ad5033cfd2d9ef210178e73bce
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-05-09 15:52:13 -0300

    Removes obsolete animation attempt

--- src/views/Speakers/index.js
+++ src/views/Speakers/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, {Component} from 'react'
+import React from 'react'
 
 import './Speakers.sass'
 
@@ -11,49 +11,32 @@ import Speaker from './Speaker'
 // DATA ________________________________________________________________________
 import speakers from './data'
 
-class Speakers extends Component {
-  state: {
-    shouldAppear: boolean
-  }
+// componentDidMount() {
+//   window.addEventListener('scroll', (e) => {
+//     const should = this.refs._grid.getBoundingClientRect().top < (window.innerHeight/2)
+//
+//     if(should && !this.state.shouldAppear) {
+//       console.log('triggered')
+//       this.setState({
+//         shouldAppear: true
+//       })
+//     }
+//   })
+// }
 
-  constructor(props: any) {
-    super(props)
-
-    this.state = {
-      shouldAppear: false
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', (e) => {
-      const should = this.refs._grid.getBoundingClientRect().top < (window.innerHeight/2)
-
-      if(should && !this.state.shouldAppear) {
-        console.log('triggered')
-        this.setState({
-          shouldAppear: true
-        })
-      }
-    })
-  }
-
-  render() {
-    // const {
-    //   shouldAppear
-    // } = this.state
-    return (
-      <Section id="speakers" name="Disertantes" className="background_orange speakers">
-        <div ref="_grid"
-          className={`speakers__grid speakers__grid--mount`}>
-          {
-            speakers.map((speaker, i) => (
-              <Speaker key={i} {...speaker}/>
-            ))
-          }
-        </div>
-      </Section>
-    )
-  }
+function Speakers() {
+  return (
+    <Section id="speakers" name="Disertantes" className="background_orange speakers">
+      <div ref="_grid"
+        className={`speakers__grid speakers__grid--mount`}>
+        {
+          speakers.map((speaker, i) => (
+            <Speaker key={i} {...speaker}/>
+          ))
+        }
+      </div>
+    </Section>
+  )
 }
 
 export default Speakers
