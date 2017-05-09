79390e427e09be3b729ac04e1c26ff900f7ff388
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-05-09 15:23:24 -0300

    Add hotel

--- src/views/Location/index.js
+++ src/views/Location/index.js
@@ -25,6 +25,27 @@ function Location() {
           <img className="tile__map" src={map} alt="Mapa de la ubicacion"/>
         </div>
       </article>
+      <article className="tile tile--hotel">
+        <h2 className="tile__title">ALOJAMIENTO</h2>
+        <p className="text-container hotel__text">El Hotel Ushuaia ofrece una tarifa diferencial a los asistentes al Simposio, la cual podrá utilizarse informando al momento de realizar la reserva la participación en el evento:
+        </p>
+        <ul className="text-container">
+          <li>Habitación single por noche $900</li>
+          <li>Habitación doble por noche $1.150</li>
+          <li>Habitación triple por noche $1.300</li>
+        </ul>
+        <ul className="tile__social hotel__links">
+          <li>
+            <a href="http://www.hotelushuaia.com.ar">www.hotelushuaia.com.ar</a>
+          </li>
+          <li>
+            <a href="tel:+54 2901 430671">+54 2901 430671</a>
+          </li>
+          <li>
+            <a href="mailto:reservas@hotelushuaia.com.ar">reservas@hotelushuaia.com.ar</a>
+          </li>
+        </ul>
+      </article>
     </Section>
   )
 }
