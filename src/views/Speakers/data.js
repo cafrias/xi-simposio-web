dfea359a21bff62dc3956e97ce348423ec888249
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-05-10 13:54:11 -0300

    Add speakers info

--- src/views/Speakers/data.js
+++ src/views/Speakers/data.js
@@ -1,32 +1,52 @@
+import pic01 from './_assets/01.jpg'
+import pic02 from './_assets/02.jpg'
+import pic04 from './_assets/04.jpg'
+import pic05 from './_assets/05.jpg'
+import pic06 from './_assets/06.jpg'
+import pic07 from './_assets/07.jpg'
+import pic08 from './_assets/08.jpg'
+
 export default [
   {
-    pic: 'http://lorempixel.com/200/200/people',
-    name: 'Ricardo Barrera',
-    inst: '(UNTDF-UNPSJB)'
-  },
-  {
-    pic: 'http://lorempixel.com/200/200/people',
-    name: 'Enrique Fowler Newton',
+    id: '01',
+    pic: pic01,
+    name: 'Ricardo Pahlen Acuña',
     inst: '(UBA)'
   },
   {
-    pic: 'http://lorempixel.com/200/200/people',
-    name: 'Juan Permuy',
-    inst: '(UBA)'
-  },
-  {
-    pic: 'http://lorempixel.com/200/200/people',
+    id: '02',
+    pic: pic02,
     name: 'Ana María Campo',
     inst: '(UBA)'
   },
   {
-    pic: 'http://lorempixel.com/200/200/people',
+    id: '04',
+    pic: pic04,
+    name: 'Ricardo Barrera',
+    inst: '(UNTDF-UNPSJB)'
+  },
+  {
+    id: '05',
+    pic: pic05,
     name: 'Francisco Provenzani',
     inst: '(UBA)'
   },
   {
-    pic: 'http://lorempixel.com/200/200/people',
-    name: 'Oscar Botino',
+    id: '06',
+    pic: pic06,
+    name: 'Hugo Bottino',
+    inst: '(UBA)'
+  },
+  {
+    id: '07',
+    pic: pic07,
+    name: 'Juan Permuy',
+    inst: '(UBA)'
+  },
+  {
+    id: '08',
+    pic: pic08,
+    name: 'Mauricio Lambertucci',
     inst: '(UBA)'
   }
 ]
